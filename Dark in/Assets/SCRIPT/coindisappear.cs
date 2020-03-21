f40af01c1d50bd6fdf72923f50ed25a2c8dad5d7
Saikarthi <41743913+Saikarthi@users.noreply.github.com>
2020-03-22 00:17:40 +0530

    movement

---
+++ Dark in/Assets/SCRIPT/coindisappear.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class coindisappear : MonoBehaviour
+{
+    public Text a;
+    public float sc;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+       
+     
+            Destroy(gameObject);
+        sc = int.Parse(a.text);
+
+        sc += 1;
+            a.text=sc.ToString("0");
+       
+       
+    }
+}
